c902f2fa1e3019271db51d34e72d1c4b2c62ac23
ShivaniAnandan <124684724+ShivaniAnandan@users.noreply.github.com>
2023-09-15 21:24:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
 //your code here
+function makeid(l,allcharacters) {
+  // write your code here
+	let idArray = allcharacters;
+	let randomId = ""
+	for (let i = 0; i < l; i++) {
+		randomId+= allcharacters[Math.floor(Math.random()*allcharacters.length)];
+	}
+	return randomId;
+}
+
+const allCharacters = [];
+for (let i = 97; i <= 122; i++) {
+  allCharacters.push(String.fromCharCode(i));
+}
+for (let i = 65; i <= 90; i++) {
+  allCharacters.push(String.fromCharCode(i));
+}
+for (let i = 1; i <= 9; i++) {
+  allCharacters.push(i.toString());
+}
+
+console.log(allCharacters);
+
+// Do not change the code below.
+const l = prompt("Enter a number.");
+
+alert(makeid(l,allCharacters));
